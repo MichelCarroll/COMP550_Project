e9e633d910f7f4b4d984e50147bd808948da365b
Nowtryz <nowtryz.dev@gmail.com>
2023-12-19 09:00:59 -0500

    Add neural baseline

--- src/neural/dataset.py
+++ src/neural/dataset.py
@@ -1,20 +1,22 @@
+import torch
 from torch.utils.data import Dataset
-
-from src.dataset_utils import MotleyFoolDataset
+from transformers import PreTrainedTokenizerBase, BatchEncoding
+from tqdm.auto import tqdm
 
 
 class TMFDatasetAdapter(Dataset):
-    def __init__(self, data, transform=None):
+    def __init__(self, data, tokenizer: PreTrainedTokenizerBase, device: torch.device = None):
         super().__init__()
 
+        self.tokenizer = tokenizer
+        self.device = device
         self.data = [
-            (self._preprocess_data(instance['content']), instance['direction'])
-            for instance in data
+            (self._preprocess_data(instance['content']), 1 if instance['direction'] == 'UP' else 0)
+            for instance in tqdm(data)
         ]
 
-    @staticmethod
-    def _preprocess_data(content):
-        return [
+    def _preprocess_data(self, content) -> BatchEncoding:
+        stripped_content = [
             para.strip().replace(' -- ', ', ')  # Replace speakers' double dash by a simple coma for simplification
             # Split text on double line ending to detect paragraphs, particularity of our data
             for para in content.split('\n\n')
@@ -22,6 +24,13 @@ class TMFDatasetAdapter(Dataset):
             # the content of the sentence. However, we still want to know when the operator is speaking
             if len(para) > 25 or "operator" in para.lower()
         ]
+
+        encodings = self.tokenizer(stripped_content, return_tensors="pt", padding=True, truncation=True)
+
+        if self.device:
+            encodings = encodings.to(self.device)
+
+        return encodings
 
     def __len__(self):
         return len(self.data)
--- src/neural/model.py
+++ src/neural/model.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F  # noqa
-from transformers import BertModel, BertTokenizerFast
+from transformers import BertModel, BertTokenizerFast, BatchEncoding
 
 
 class NeuralNet(nn.Module):
@@ -41,20 +41,21 @@ class NeuralNet(nn.Module):
     def tokenize(self, *args, **kwargs):
         return self.bert_tokenizer.tokenize(*args, **kwargs)
 
-    def _encode_transcript(self, transcript: list[str]) -> torch.Tensor:
+    def _encode_transcript(self, transcript: BatchEncoding) -> torch.Tensor:
         """
         Encode the transcript to the embedding space of the model
         :param transcript: The transcript to encode as a list of paragraphs
         :return: Transcript embedding, size lstm_output_size
         """
-        input_seq = self.bert_tokenizer(transcript, return_tensors="pt", padding=True)
+        # input_seq = self.bert_tokenizer(transcript, return_tensors="pt", padding=True)
+        input_seq = transcript
         output = self.fin_bert(**input_seq)
         cls_tokens = output.last_hidden_state[:, 0]  # sequence length (paras) x hidden size
         lstm_input = cls_tokens.flip(0)  # Invert sequence to perform bottom-up then to-down LSTM
         _, (h_n, _) = self.lstm(lstm_input, (self.h_0, self.c_0))
         return h_n.view(-1)
 
-    def forward(self, batch: list[list[str]]) -> torch.Tensor:
+    def forward(self, batch: list[BatchEncoding]) -> torch.Tensor:
         """
         Forward pass of the Model
         :param batch: input as (BatchSize, Paras)
